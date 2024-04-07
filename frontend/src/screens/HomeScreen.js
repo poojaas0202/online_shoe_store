f04774fa0368278a3a06c5b1911b0add59f5c83e
poojaas0202 <141813947+poojaas0202@users.noreply.github.com>
2024-04-07 12:02:26 +0530

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -36,7 +36,7 @@ function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get("/api/products");
+        const result = await axios.get("https://online-shoe-store-server.vercel.app/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
